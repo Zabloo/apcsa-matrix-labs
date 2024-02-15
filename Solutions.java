9665319c4c36d3da1378591946aa8b1f516e064c
Nader Hallik <naderhallik@gmail.com>
2024-02-15 11:20:04 -0500

    Finished, made multiply method

--- Solutions.java
+++ Solutions.java
@@ -1,5 +1,6 @@
 public class Solutions {
     public static void main(String[] args) {
+        System.out.println("yes");
     /*
         int[][] arr = new int[][] {{1, 2, 3}, {2, 13, 1}, {6, 5, 100}};
         int[][] newArr = scale(arr, 3);
@@ -7,10 +8,23 @@ public class Solutions {
         */
         // System.out.println(10 - Math.floor(Math.log10(5)));
         // int[][] matrix = new int[][] {{6, 4, 2}, {1, -2, 8}, {1, 5, 7}};
+        /*
         int[][] matrix = new int[][] {{6, 4, 2}, {1, -2, 8}, {1, 5, 7}};
         printMatrix(matrix);
         try {
             System.out.println("Determinant: " + determinant(matrix));
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        */
+        int[][] m1 = {{1, 4}, {2, 5}, {3, 6}};
+        printMatrix(m1);
+        System.out.println();
+        int[][] m2 = {{7, 9, 11}, {8, 10, 12}};
+        printMatrix(m2);
+        System.out.println();
+        try {
+            printMatrix(multiplyMatrices(m1, m2));
         } catch (Exception e) {
             e.printStackTrace();
         }
@@ -25,6 +39,7 @@ public class Solutions {
         }
         return output;
     }
+
 
     public static int determinant(int[][] input) throws Exception {
         // System.out.println("iteratio");
@@ -54,9 +69,28 @@ public class Solutions {
         return result;
     }
 
+
+    public static int[][] multiplyMatrices(int[][] m1, int[][] m2) throws Exception {
+        if (m1.length != m2[0].length || m1[0].length != m2.length) {
+            if (m2.length == m1[0].length && m1.length == m2[0].length)
+                return multiplyMatrices(m2, m1); // reverse inputs
+            throw new Exception("Number of rows in one matrix must equal number of columns in the other, and vice versa.");
+        }
+        int[][] result = new int[m1[0].length][m2.length]; // m2.length (num of cols in m2) is same as m1[0].length (num of rows in m2)
+        for (int m1Row = 0; m1Row < m1[0].length; m1Row++) {
+            for (int m2Col = 0; m2Col < m2.length; m2Col++) {
+                for (int a = 0; a < m2.length + 1; a++) { // m2.length (num of cols in m2) is same as m1[0].length (num of rows in m2)
+                    result[m2Col][m1Row] += m1[a][m1Row] * m2[m2Col][a];
+                }
+            }
+        }
+        return result;
+    }
+
+
     public static void printMatrix(int[][] matrix) {
-        for (int row = 0; row < matrix.length; row++) {
-            for (int col = 0; col < matrix[0].length; col++) {
+        for (int row = 0; row < matrix[0].length; row++) {
+            for (int col = 0; col < matrix.length; col++) {
                 System.out.print(matrix[col][row] + " ");
             }
             System.out.println();
